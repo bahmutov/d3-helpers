6f1fd1edb842aa1013ab5e7bfb3b2ff44f369dae
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-04-02 22:21:54 -0400

    adding index.html, registering as window object

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-var helpers = {
-  noop: function () {},
-  no: function () { return false; },
-  yes: function () { return true; },
-  pass: function (d) { return d; },
-  datum: function (d) { return d; },
-  index: function (d, i) { return i; },
-  value: function (val) {
-    return function () {
-      return val;
-    };
-  },
-  property: function (name, fn) {
-    return function (d) {
-      return (typeof fn === 'function' ? fn(d[name]) : d[name]);
-    };
-  }
-};
+(function () {
 
-module.exports = helpers;
+  var helpers = (function () {
+    var helpers = {
+      noop: function () {},
+      no: function () { return false; },
+      yes: function () { return true; },
+      pass: function (d) { return d; },
+      datum: function (d) { return d; },
+      index: function (d, i) { return i; },
+      value: function (val) {
+        return function () {
+          return val;
+        };
+      },
+      property: function (name, fn) {
+        return function (d) {
+          return (typeof fn === 'function' ? fn(d[name]) : d[name]);
+        };
+      }
+    };
+
+    return helpers;
+  }());
+
+  if (typeof window === 'object') {
+    /* global window */
+    window.d3h = helpers;
+  } else if (typeof module === 'object') {
+    module.exports = helpers;
+  } else {
+    throw new Error('Do not know how to exports D3 helpers');
+  }
+
+}());
